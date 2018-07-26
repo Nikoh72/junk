e550238517d72f3d2f56e9010842e95454641498
Your Name <luvativickram@gmail.com>
2018-07-26 14:04:59 +0300

    fir

---
+++ app/src/main/java/com/example/emobilis/pid/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.emobilis.pid;
+
+import android.content.Intent;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+public class MainActivity extends AppCompatActivity {
+    private GestureDetectorCompat mymotion;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mymotion=new GestureDetectorCompat(this,new learnGesture());
+    }
+    public boolean onTouchEvent(MotionEvent event){
+        this.mymotion.onTouchEvent(event);
+        return super.onTouchEvent(event);
+}
+
+    class learnGesture extends GestureDetector.SimpleOnGestureListener {
+        //a simple gesture listener is to tell which gesturehas been made
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            if (e2.getX() > e1.getX()){
+                //this is if u swipe from left to right
+                Intent left=new Intent(getApplicationContext(),Main2Activity.class);
+                finish();
+                //finish it to stop main activity
+                startActivity(left);
+
+
+            }
+
+
+
+
+            return true;
+
+        }} }
